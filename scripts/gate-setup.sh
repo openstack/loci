f3f0ce7ef259cc95ccca803d4fe1abe288dee780
Sam Yaple <sam@yaple.net>
2017-03-30 21:00:30 +0000

    Fix gate scripts
    With these changes we get successful runs in subprojects
    
    Change-Id: I8ac18de3ea08ca9224e9e15b82a1ce3048c5edec

--- scripts/gate-setup.sh
+++ scripts/gate-setup.sh
@@ -23,15 +23,30 @@ function setup_docker {
     sudo apt-get install --no-install-recommends -y docker-engine
 
     sudo systemctl stop docker
-    sudo mount -t tmpfs tmpfs /var/lib/docker
+    sudo mount -o size=25g -t tmpfs tmpfs /var/lib/docker
+    # TODO(SamYaple): Images cannot be build with userns-remap enabled. figure that out
     sudo tee /etc/systemd/system/docker.service << EOF
 [Service]
 ExecStart=
-ExecStart=/usr/bin/dockerd --userns-remap default --storage-driver overlay2 --group jenkins
+ExecStart=/usr/bin/dockerd --storage-driver overlay2 --group jenkins
 EOF
     sudo systemctl daemon-reload
     sudo systemctl start docker
 }
 
+function setup_swap {
+    sudo fallocate -l20G /swap
+    sudo chmod 0600 /swap
+    sudo mkswap /swap
+    sudo swapon /swap
+}
+
+# TODO(SamYaple): Should this be done in infra?
+function clone_project {
+    /usr/zuul-env/bin/zuul-cloner --cache-dir /opt/git git://git.openstack.org $ZUUL_PROJECT
+}
+
 debug_info
+setup_swap
 setup_docker
+clone_project
