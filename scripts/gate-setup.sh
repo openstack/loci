0f4969682897a414535bafe5793f5351dfc8d0f5
Sam Yaple <sam@yaple.net>
2017-04-05 20:01:27 +0000

    Update userns info
    Change-Id: I591dca72212bb673bcc03dd33959ef98458a8112

--- scripts/gate-setup.sh
+++ scripts/gate-setup.sh
@@ -24,7 +24,9 @@ function setup_docker {
 
     sudo systemctl stop docker
     sudo mount -o size=25g -t tmpfs tmpfs /var/lib/docker
-    # TODO(SamYaple): Images cannot be build with userns-remap enabled. figure that out
+    # TODO(SamYaple): CentOS cannot be build with userns-remap enabled. httpd
+    # uses cap_set_file capability and there is no way to pass that in at build
+    # time yet.
     sudo tee /etc/systemd/system/docker.service << EOF
 [Service]
 ExecStart=
