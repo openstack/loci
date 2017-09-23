bdb298f4358e4bf9334a9dbafc6e9523a192e4ce
Sam Yaple <sam@yaple.net>
2017-09-22 21:43:07 -0400

    Use new bindep syntax
    With new bindep syntax we can AND together specific profiles. The most
    blatant need for this is with 'python-rbd'. We should only install
    python-rbd when both 'ceph' and 'glance' profiles are specified.
    
    The change is merged upstream, but a new version of bindep is not tagged
    yet, so we install from upstream temporarily. Using hte upstream package
    will not effect the output of the image.
    
    With this change we can use the same profiles across all projects when
    building, so the user only needs to set one list of profiles rather than
    profiles per project which could help pipelines keep images consistent.
    
    export PROFILES="ceph openvswitch nginx"
    docker build --build-arg PROFILES="${PROFILES}" --build-arg PROJECT=keystone .
    docker build --build-arg PROFILES="${PROFILES}" --build-arg PROJECT=glance .
    docker build --build-arg PROFILES="${PROFILES}" --build-arg PROJECT=nova .
    docker build --build-arg PROFILES="${PROFILES}" --build-arg PROJECT=neutron .
    
    Change-Id: I50a73bfab9c80c8650f5f59c4e16fdfd32fc9c47

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -2,101 +2,9 @@
 
 set -eux
 
-# TODO(SamYaple): Switch all of this to bindep once syntax is supported better
-# NOTE(SamYaple): Ubuntu and Debian have slightly different package lists
-case ${distro} in
-    debian)
-        apt-get update
-        apt-get upgrade -y
-        apt-get install -y --no-install-recommends \
-            build-essential \
-            ca-certificates \
-            git \
-            liberasurecode-dev \
-            libffi-dev \
-            libkrb5-dev \
-            libldap2-dev \
-            libmariadbclient-dev \
-            libnss3-dev \
-            libpq-dev \
-            libsasl2-dev \
-            libssl-dev \
-            libsystemd-dev \
-            libxml2-dev \
-            libxslt1-dev \
-            libvirt-dev \
-            libyaml-dev \
-            libz-dev \
-            pkg-config \
-            python-dev \
-            python-pip \
-            python-virtualenv
-        ;;
-    ubuntu)
-        apt-get update
-        apt-get upgrade -y
-        apt-get install -y --no-install-recommends \
-            build-essential \
-            ca-certificates \
-            git \
-            liberasurecode-dev \
-            libffi-dev \
-            libkrb5-dev \
-            libldap2-dev \
-            libmysqlclient-dev \
-            libnss3-dev \
-            libpq-dev \
-            libsasl2-dev \
-            libssl-dev \
-            libsystemd-dev \
-            libxml2-dev \
-            libxslt1-dev \
-            libvirt-dev \
-            libyaml-dev \
-            libz-dev \
-            pkg-config \
-            python-dev \
-            python-pip
-        ;;
-    centos)
-        yum upgrade -y
-        # NOTE(SamYaple): https://bugs.centos.org/view.php?id=10750
-        yum install -y --setopt=tsflags=docs --setopt=skip_missing_names_on_install=False libffi-devel
-        yum install -y --setopt=skip_missing_names_on_install=False \
-            gcc \
-            gcc-c++ \
-            make \
-            openssl-devel \
-            ca-certificates \
-            git \
-            bzip2 \
-            liberasurecode-devel \
-            openldap-devel \
-            mariadb-devel \
-            nss-devel \
-            postgresql-devel \
-            cyrus-sasl-devel \
-            openssl-devel \
-            libxml2-devel \
-            libxslt-devel \
-            libvirt-devel \
-            libyaml-devel \
-            zlib-devel \
-            pkgconfig \
-            python \
-            python-devel \
-            python-pip \
-            libgcrypt \
-            nss-util \
-            systemd-devel
-        ;;
-    *)
-        echo "Unknown distro: ${distro}"
-        exit 1
-        ;;
-esac
-
 $(dirname $0)/setup_pip.sh
+pip install git+https://github.com/openstack-infra/bindep
+$(dirname $0)/install_packages.sh
 $(dirname $0)/clone_project.sh
 mv /tmp/requirements/{global-requirements.txt,upper-constraints.txt} /
 
@@ -107,7 +15,7 @@ split -l1 /upper-constraints.txt
 ls -1 | xargs -n1 -P20 -t pip wheel --no-deps --wheel-dir / -c /upper-constraints.txt -r
 popd
 # NOTE(SamYaple): Handle packages not in global-requirements
-additional_packages=(argparse bindep==2.5.0 pip setuptools uwsgi wheel virtualenv)
+additional_packages=(argparse git+https://github.com/openstack-infra/bindep pip setuptools uwsgi wheel virtualenv)
 echo "${additional_packages[@]}" | xargs -n1 -P20 pip wheel --wheel-dir / -c /upper-constraints.txt
 
 # NOTE(SamYaple): We want to purge all files that are not wheels or txt to
