b3b26b7b4b85b45b38722a701041968cb1ad3dba
Sam Yaple <sam@yaple.net>
2017-10-21 00:18:05 -0400

    Ignore wheels that exist natively whl upstream
    This prevents ~200MB of needless pypi downloading and speeds up the
    build process, esspecially in environments without a PyPI mirror.
    
    Change-Id: Icb36ad752923730f5daca22ef08455551e1161a8

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -14,23 +14,34 @@ if (( $(openssl version | awk -F'[ .]' '{print $3}') >= 1 )); then
     sed -i '/python-qpid-proton/d' /upper-constraints.txt
 fi
 
+if [[ "${PYTHON3}" == "no" ]]; then
+    ignore_wheels=py2
+else
+    ignore_wheels=py3
+fi
+
+pushd $(mktemp -d)
+# NOTE(SamYaple): Exclude known native wheels from list
+cp /upper-constraints.txt tmp-upper-constraints.txt
+cat $(dirname $0)/ignored_wheels{,_${ignore_wheels}} | xargs -n1 -I{} sed -i '/^{}=/d' tmp-upper-constraints.txt
+
 # NOTE(SamYaple): Build all deps in parallel. This is safe because we are
 # constrained on the version and we are building with --no-deps
 export CASS_DRIVER_BUILD_CONCURRENCY=8
-pushd $(mktemp -d)
-split -l1 /upper-constraints.txt
+split -l1 tmp-upper-constraints.txt
 ls -1 | xargs -n1 -P20 -t pip wheel --no-deps --wheel-dir / -c /upper-constraints.txt -r | tee /tmp/wheels.txt
 popd
 # NOTE(SamYaple): Handle packages not in upper-constriants and not in PyPI as
 # native whls
 additional_packages=(git+https://github.com/openstack-infra/bindep@24427065c5f30047ac80370be0a390e7f417ce34 uwsgi)
-echo "${additional_packages[@]}" | xargs -n1 -P20 pip wheel --wheel-dir / -c /upper-constraints.txt
+echo "${additional_packages[@]}" | xargs -n1 -P20 pip wheel --wheel-dir / -c /upper-constraints.txt | tee -a /tmp/wheels.txt
 
 # NOTE(SamYaple) Remove native-binary wheels, we only want to keep wheels that
-# we compiled ourselves
-# TODO(SamYaple): Figure out how to not download a whl if it already exists in
-# whl form upstream
-awk -F'[ ,]+' '/^Skipping/ {gsub("-","_");print $2}' /tmp/wheels.txt | xargs -n1 bash -c 'ls /$1-*' _ | xargs rm
+# we compiled ourselves. We should not have any of these, but as packages
+# convert to wheels overtime some will slip through
+# TODO(SamYaple): Periodically check that no additional wheels are
+# inappropriately downloaded
+awk -F'[ ,]+' '/^Skipping/ {gsub("-","_");print $2}' /tmp/wheels.txt | xargs -r -n1 bash -c 'ls /$1-*' _ | sort -u | xargs -t -r rm
 
 # NOTE(SamYaple): We want to purge all files that are not wheels or txt to
 # reduce the size of the layer to only what is needed
