5a265324b9dbefb672d2978cd7aa411054f1a9e0
Rudolf Vriend <rudolf.vriend@sap.com>
2018-01-15 12:25:24 +0100

    added missing path for custom-requirements.txt

--- scripts/pip_install.sh
+++ scripts/pip_install.sh
@@ -7,6 +7,6 @@ packages=$@
 pip install --no-cache-dir --only-binary :all: --no-compile -c /tmp/wheels/upper-constraints.txt --find-links /tmp/wheels/ ${packages}
 
 # add custom requirements
-if [[ -e ${PROJECT}/custom-requirements.txt ]]; then
-    pip install --no-cache-dir -r ${PROJECT}/custom-requirements.txt -c /tmp/wheels/upper-constraints.txt
+if [[ -e /tmp/${PROJECT}/custom-requirements.txt ]]; then
+    pip install --no-cache-dir -r /tmp/${PROJECT}/custom-requirements.txt -c /tmp/wheels/upper-constraints.txt
 fi
