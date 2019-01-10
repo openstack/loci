4c25400ef96d945717ff215b150d127d3f03ea9c
Rudolf Vriend <rudolf.vriend@sap.com>
2019-01-10 11:35:46 +0100

    changed sequence of requirements pip install

--- scripts/pip_install.sh
+++ scripts/pip_install.sh
@@ -4,13 +4,14 @@ set -ex
 
 packages=$@
 
+# install custom requirements first, so they can provide existing, installed dependencies for pip install {packages} in the next step
+if [[ -e /tmp/${PROJECT}/custom-requirements.txt ]]; then
+    pip install --no-cache-dir --pre --no-compile -r /tmp/${PROJECT}/custom-requirements.txt -c /tmp/wheels/upper-constraints.txt --find-links /tmp/wheels/ ${PIP_ARGS}
+fi
+
 if [[ "${PROJECT}" == "rally-openstack" ]]; then
     pip install --no-cache-dir --only-binary :all: --no-compile -c /tmp/${PROJECT}/upper-constraints.txt --find-links /tmp/wheels/ ${PIP_ARGS} ${packages}
 else
     pip install --no-cache-dir --pre --only-binary :all: --no-compile -c /tmp/wheels/upper-constraints.txt --find-links /tmp/wheels/ ${PIP_ARGS} ${packages}
 fi
 
-# add custom requirements
-if [[ -e /tmp/${PROJECT}/custom-requirements.txt ]]; then
-    pip install --no-cache-dir --pre --no-compile -r /tmp/${PROJECT}/custom-requirements.txt -c /tmp/wheels/upper-constraints.txt --find-links /tmp/wheels/ ${PIP_ARGS}
-fi
