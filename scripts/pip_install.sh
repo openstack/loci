2f4e581f03ca13fcd20f31c9025d8cced89d04d3
Rudolf Vriend <rudolf.vriend@sap.com>
2019-01-10 09:31:43 +0100

    also consider non-stable versions during pip install (our development artefacts in the wheels distribution)

--- scripts/pip_install.sh
+++ scripts/pip_install.sh
@@ -7,10 +7,10 @@ packages=$@
 if [[ "${PROJECT}" == "rally-openstack" ]]; then
     pip install --no-cache-dir --only-binary :all: --no-compile -c /tmp/${PROJECT}/upper-constraints.txt --find-links /tmp/wheels/ ${PIP_ARGS} ${packages}
 else
-    pip install --no-cache-dir --only-binary :all: --no-compile -c /tmp/wheels/upper-constraints.txt --find-links /tmp/wheels/ ${PIP_ARGS} ${packages}
+    pip install --no-cache-dir --pre --only-binary :all: --no-compile -c /tmp/wheels/upper-constraints.txt --find-links /tmp/wheels/ ${PIP_ARGS} ${packages}
 fi
 
 # add custom requirements
 if [[ -e /tmp/${PROJECT}/custom-requirements.txt ]]; then
-    pip install --no-cache-dir --no-compile -r /tmp/${PROJECT}/custom-requirements.txt -c /tmp/wheels/upper-constraints.txt --find-links /tmp/wheels/ ${PIP_ARGS}
+    pip install --no-cache-dir --pre --no-compile -r /tmp/${PROJECT}/custom-requirements.txt -c /tmp/wheels/upper-constraints.txt --find-links /tmp/wheels/ ${PIP_ARGS}
 fi
