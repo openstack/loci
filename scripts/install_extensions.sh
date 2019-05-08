5389ef7c4e6db7f3e3ebb0ebf4c97673c3f1bad4
Rudolf Vriend <rudolf.vriend@sap.com>
2019-05-08 17:23:54 +0200

    reverted. APIC_URL will be set to swift share instead

--- scripts/install_extensions.sh
+++ scripts/install_extensions.sh
@@ -25,11 +25,10 @@ $(dirname $0)/fetch_wheels.sh
 
 $(dirname $0)/clone_project.sh
 
-# should now come from https://objectstore-3.eu-de-1.cloud.sap/v1/AUTH_caa6209d2c38450f8266311fd0f05446/sapcc-pypi/aci-python-bindings/
 # special neutron aci drivers installation sauce
-#if [[ ${PROJECT} == 'neutron' ]]; then
-#    $(dirname $0)/install_apic.sh
-#fi
+if [[ ${PROJECT} == 'neutron' ]]; then
+    $(dirname $0)/install_apic.sh
+fi
 
 $(dirname $0)/pip_install_extensions.sh
 $(dirname $0)/cleanup.sh
