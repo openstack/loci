60b5a3009892ba67b990014efd97c00aacbd416d
Rudolf Vriend <rudolf.vriend@sap.com>
2019-05-09 11:16:12 +0200

    swift renders href urls in quotes..

--- scripts/install_apic.sh
+++ scripts/install_apic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-curl -s --noproxy "*" -k ${APIC_URL}  | grep "href.*\.egg" | sed "s#.*href=\([^-]*\)\([^>]*\).*#url=${APIC_URL}\1\2\\noutput=\1.egg#g" | curl -k -K -
+curl -s --noproxy "*" -k ${APIC_URL}  | grep "href.*\.egg" | sed "s#.*href=\"\([^-]*\)\([^>]*\)\".*#url=${APIC_URL}\1\2\\noutput=\1.egg#g" | curl -k -K -
 for f in *.egg; do
     easy_install -O2 $f
     rm $f
