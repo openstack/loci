690c3d7673ff1a48b2ac787505bffaf621d86eba
Andrii Ostapenko <aostapenko@mirantis.com>
2020-01-27 15:23:24 -0600

    Remove an entry for building project from uc
    In some releases (Stein, Train, Rocky) uc contains entries for neutron
    and horizon, that breaks build for these components with unsatisfied
    constraints error. Obviously we don't need this constraint for the
    component we build.
    
    Change-Id: If490aacc6df6198c286a4b316165892452db8ab5

--- scripts/fetch_wheels.sh
+++ scripts/fetch_wheels.sh
@@ -14,3 +14,8 @@ mkdir -p /tmp/wheels/
 # Exclude all files starting with '.' as these can be control files for
 # AUFS which have special meaning on AUFS backed file stores.
 tar xf /tmp/wheels.tar.gz --exclude='.*' -C /tmp/wheels/
+
+# Presence of constraint for project we build (in Stein, Train for Horizon and
+# Neutron) in uc breaks project installation with unsatisfied constraints error
+# This line ensures that such constraint is absent for any future occurrence
+sed -i "/^${PROJECT}===/d" /tmp/wheels/upper-constraints.txt
