97d1eb8e9d06ca38129d510eceef3450ab444302
Johannes Kulik <johannes.kulik@sap.com>
2020-10-02 12:03:12 +0200

    Support keppel's auth URL
    Keppel seems to not implement "/v2/token" for getting a token, but
    instead has a custom URL "/keppel/v1/auth" for this purpose.

--- scripts/fetch_wheels.py
+++ scripts/fetch_wheels.py
@@ -14,12 +14,15 @@ DOCKER_REGISTRY='registry.hub.docker.com'
 
 def get_token(protocol, registry, repo):
     if registry == DOCKER_REGISTRY:
-      authserver = 'auth.docker.io'
+      authserver = 'auth.docker.io/token'
       service = 'registry.docker.io'
+    elif registry.startswith('keppel'):
+      authserver = '{}/keppel/v1/auth'.format(registry)
+      service = registry.split(':')[0]
     else:
-      authserver = "{}/v2".format(registry)
-      service =  registry.split(':')[0]
-    url = "{}://{}/token?service={}&" \
+      authserver = "{}/v2/token".format(registry)
+      service = registry.split(':')[0]
+    url = "{}://{}?service={}&" \
             "scope=repository:{}:pull".format(protocol, authserver, service, repo)
     print(url)
     try:
