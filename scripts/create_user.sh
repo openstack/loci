75b269d8d14bee771f8eb9ac6c0bc93cfc2c216b
liyingjun <yinjalee@163.com>
2023-04-25 00:58:23 +0000

    fix(nova): enable login ability for nova resizing or migrating
    Login should be enabled for nova resizing or migrating between
    hypervisors[1].
    
    [1]: https://docs.openstack.org/nova/latest/admin/ssh-configuration.html
    
    Change-Id: I1db692c3fbb371873ebe5432495130f4ecfdc930

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -3,7 +3,12 @@
 set -ex
 
 groupadd -g ${GID} ${PROJECT}
-useradd -u ${UID} -g ${PROJECT} -M -d /var/lib/${PROJECT} -s /usr/sbin/nologin -c "${PROJECT} user" ${PROJECT}
+if [[ "${PROJECT}" == "nova" ]];then
+    # NOTE: bash needed for nova to support instance migration
+    useradd -u ${UID} -g ${PROJECT} -M -d /var/lib/${PROJECT} -s /bin/bash -c "${PROJECT} user" ${PROJECT}
+else
+    useradd -u ${UID} -g ${PROJECT} -M -d /var/lib/${PROJECT} -s /usr/sbin/nologin -c "${PROJECT} user" ${PROJECT}
+fi
 
 mkdir -p /etc/${PROJECT} /var/log/${PROJECT} /var/lib/${PROJECT} /var/cache/${PROJECT}
 chown ${PROJECT}:${PROJECT} /etc/${PROJECT} /var/log/${PROJECT} /var/lib/${PROJECT} /var/cache/${PROJECT}
